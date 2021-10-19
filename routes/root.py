89a72eccea1bd3fdc9507bc2300c8c8d6f48d12b
moshiur251 <moshiur251@gmail.com>
2021-10-19 00:41:52 +0000

    new route added

--- routes/root.py
+++ routes/root.py
@@ -1,8 +1,8 @@
 from typing import List
 from fastapi import APIRouter, Depends
-# from config.db import db
-# from config.auth import get_current_active_user
-# from schemas.user import UserInfo
+from config.db import db
+from config.librarian_auth_config import get_current_active_user
+from schemas.student import StudentInfo
 
 
 router = APIRouter()
@@ -12,11 +12,7 @@ router = APIRouter()
 async def root():
     return {"response": "Welcome to your app!"}
 
-# @router.get('/api/users', response_model=List[UserInfo], response_description="List of all users")
-# async def get_all_users(current_user: UserInfo = Depends(get_current_active_user)):
-#     users = await collection.find().to_list(100)
-#     return users
 
-# @router.get("/api/users/me/", response_model=UserInfo)
-# async def read_users_me(current_user: UserInfo = Depends(get_current_active_user)):
-#     return current_user
+@router.get("/api/users/me/", response_model=StudentInfo)
+async def read_users_me(current_user: StudentInfo = Depends(get_current_active_user)):
+    return current_user
